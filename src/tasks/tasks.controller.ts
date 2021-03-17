d051252cb1af76759f3a3219b49b5f8917e4999b
Adrianos SIDIRAS <adrianos.sidiras@gmail.com>
2021-03-17 16:25:08 +0100

    Second commit

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,39 +1,42 @@
-import { Body, Controller, Get, Post, Param, Delete, Patch, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Get, Post, Param, Delete, Patch, UsePipes, ValidationPipe, ParseIntPipe, Query } from '@nestjs/common';
 import { createTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
-import { Task, TaskStatus } from './tasks.model';
+import { TaskStatus } from './task-status.enum';
+import { Task } from './task.entity';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
     constructor(private tasksService: TasksService){}
-    
-    @Get()
-    getAllTasks(): Task[] {
-        return this.tasksService.getAllTasks();
-    }
 
     @Get('/:id')
-    getTaskById(@Param('id') id : string){
+    getTaskById(@Param('id', ParseIntPipe) id : number): Promise<Task>{
         return this.tasksService.getTaskById(id);
     }
 
     @Post()
     @UsePipes(ValidationPipe)
-    createTask(@Body() createTaskDto: createTaskDto): Task{
+    createTask(@Body() createTaskDto: createTaskDto): Promise<Task>{
         return this.tasksService.createTask(createTaskDto);
         
     }
 
     @Delete('/:id')
-    deleteTask(@Param('id') id: string): void{
+    deleteTask(@Param('id', ParseIntPipe) id: number): Promise<void> {
         return this.tasksService.deleteTask(id);
     }
 
     @Patch('/:id/status')
     updateTaskStatus (
-    @Param('id') id: string,
-    @Body('status', TaskStatusValidationPipe)  status : TaskStatus) : Task {
+    @Param('id', ParseIntPipe) id: number,
+    @Body('status', TaskStatusValidationPipe)  status : TaskStatus) : Promise<Task> {
         return this.tasksService.updateTaskStatus(id,status);
     }
+
+    @Get()
+    getTasks(@Query(ValidationPipe) filterDto: GetTasksFilterDto): Promise<Task[]>  {
+        return this.tasksService.getTasks(filterDto);
+    }
+    
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,12 +1,55 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { Task, TaskStatus } from './tasks.model';
-import { v1 as uuid} from 'uuid';
+import { InjectRepository } from '@nestjs/typeorm';
 import { createTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { TaskStatus } from './task-status.enum';
+import { Task } from './task.entity';
+import { TaskRepository } from './task.repository';
 
 @Injectable()
 export class TasksService {
-    private tasks: Task[] = [];
+    constructor(
+        @InjectRepository(TaskRepository)
+        private taskRepository : TaskRepository,
+    ) { }
 
+    async getTaskById(id: number): Promise<Task>{
+        const found = await this.taskRepository.findOne(id);
+        if(!found) {
+            throw new NotFoundException(`Task with ID "${id}" not found`);
+        }
+        return found;
+    }
+
+    async createTask(createTaskDto: createTaskDto): Promise<Task> {
+        return this.taskRepository.createTask(createTaskDto);
+    }
+
+    async deleteTask(id: number): Promise<void> {
+        const result = await this.taskRepository.delete(id);
+        if(result.affected===0) {
+            throw new NotFoundException(`Task with ID "${id}" not found`);
+        }
+    }
+
+    async updateTaskStatus(id: number, status : TaskStatus): Promise<Task> {
+        const task = await this.getTaskById(id);
+        task.status = status;
+        await task.save();
+        return task;
+    }
+
+    async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+        return this.taskRepository.getTasks(filterDto);
+    }
+
+}
+
+
+
+
+
+/* 
     getAllTasks(): Task[] {
         return this.tasks;
     }
@@ -42,6 +85,5 @@ export class TasksService {
         task.status = status;
         return task;
 
-    }
-    
-}
+    } */
+
